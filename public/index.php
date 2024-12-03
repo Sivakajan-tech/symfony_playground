e3430359d6d7fea8f8714a443e6967703bc193bc
Sivakajan-tech <my.otherwrks@gmail.com>
2024-12-03 12:29:50 +0530

    fix cicd

--- public/index.php
+++ public/index.php
@@ -2,8 +2,7 @@
 
 use App\Kernel;
 
-(new Symfony\Component\Dotenv\Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
-require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
